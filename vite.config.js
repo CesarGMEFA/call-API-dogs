f6fecf81b70b76e07cc08aa2776dedd79b8f54cc
Cesar Mejias <cesarfar376@gmail.com>
2022-07-19 12:57:05 -0400

    corrigió la ruta de favoritos.html en la compilación ts

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const _dirname = typeof __dirname !== 'undefined'
+  ? __dirname
+  : dirname(fileURLToPath(import.meta.url))
+
+const outDir = resolve(_dirname, "dist");
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			outDir,
+			emptyOutDir: true,
+			input: {
+				main: resolve(_dirname, "index.html"),
+				public: resolve(_dirname, "public/favourites.html"),
+			},
+		},
+	},
+});
